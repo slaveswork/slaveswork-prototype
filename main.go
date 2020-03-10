604ee8a607f6f2a23162c32927552e810a370464
DoDaek <dbxor8960@naver.com>
2020-03-10 14:35:45 +0900

    Examples folder for prototype example

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"errors"
+	"time"
+
+	"github.com/shirou/gopsutil/cpu"
+)
+
+var errUsageOfCPU error = errors.New("Unable to measure CPU usage.")
+
 func main() {
 
+}
+
+func cpuUsage() ([]float64, error) {
+	percent, err := cpu.Percent(time.Duration(1)*time.Second, false)
+	if err != nil {
+		return nil, errUsageOfCPU
+	}
+	return percent, nil
 }
