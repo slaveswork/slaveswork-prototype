08b7a18593550c246f6634296187d9ffd63f6d84
alencion <rhqnrdl625@naver.com>
2020-04-16 16:15:14 +0900

    tab home

---
+++ ui/src/componets/tab/Device.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Device = () => {
+    return (
+        <div className="tab">
+
+        </div>
+    );
+}
+
+export default Device;
---
+++ ui/src/componets/tab/Task.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Task = () => {
+    return (
+        <div className="tab">
+
+        </div>
+    );
+}
+
+export default Task;
