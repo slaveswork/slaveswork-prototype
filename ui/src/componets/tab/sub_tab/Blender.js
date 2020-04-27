91d3891f6312794d7d62957b83b92a4984928ca5
alencion <rhqnrdl625@naver.com>
2020-04-27 15:31:34 +0900

    add blender for example

--- ui/src/componets/tab/sub_tab/Blender.js
+++ ui/src/componets/tab/sub_tab/Blender.js
@@ -3,16 +3,18 @@ import React from 'react';
 const Blender = () => {
     return (
         <div className="sub_tab">
-            For example
-            - Start Blender
-            - Select the "File->User Preferences" menu item
-            - Switch to tab "Add-ons"
-            - Press the "Install from file..." button in the bottom of the preferences dialog
-            - Select the .zip file and double-click on it
-            - In the search box, enter "bitwrk" to filter the list of add-ons shown
-            - Make sure that "Render: BitWrk Distributed Rendering" has been checked
-            - If this is an update: restart the add-on by un-checking it first
-            - Back in the main window, you should be able to switch the renderer to BitWrk
+            <h5>For example</h5>
+            <ul>
+                <li>- Start Blender</li>
+                <li>- Select the "File->User Preferences" menu item</li>
+                <li>- Switch to tab "Add-ons"</li>
+                <li>- Press the "Install from file..." button in the bottom of the preferences dialog</li>
+                <li>- Select the .zip file and double-click on it</li>
+                <li>- In the search box, enter "bitwrk" to filter the list of add-ons shown</li>
+                <li>- Make sure that "Render: BitWrk Distributed Rendering" has been checked</li>
+                <li>- If this is an update: restart the add-on by un-checking it first</li>
+                <li>- Back in the main window, you should be able to switch the renderer to BitWrk</li>
+            </ul>
         </div>
     );
 }
