5597393bdb285d916048c45f09012edae7c6ee7c
DoDaek <dbxor8960@naver.com>
2020-05-03 09:34:26 +0900

    Refactoring codes

--- message/message.go
+++ message/message.go
@@ -16,20 +16,35 @@ type AppMessage struct {
 }
 
 type AppConnectionDeviceMessage struct {
+	*gotron.Event
+	Body *AppConnectionDeviceBody `json:"body"`
+}
+
+type AppConnectionDeviceBody struct {
 	IP    string `json:"ip"`
 	Port  string `json:"port"`
 	Token string `json:"token"`
 }
 
 type WindowNetworkStatusMessage struct {
+	*gotron.Event                 `json:"event"`
+	Body *WindowNetworkStatusBody `json:"body"`
+}
+
+type WindowNetworkStatusBody struct {
 	IP   string `json:"ip"`
 	Port string `json:"port"`
 }
 
 type WindowSendTokenMessage struct {
+	*gotron.Event             `json:"event"`
+	Body *WindowSendTokenBody `json:"body"`
+}
+
+type WindowSendTokenBody struct {
 	Token string `json:"token"`
 }
 
-func (c *AppConnectionDeviceMessage) MakeHostAddress() string {
+func (c *AppConnectionDeviceBody) MakeHostAddress() string {
 	return fmt.Sprintf("%s:%s", c.IP, c.Port)
 }
