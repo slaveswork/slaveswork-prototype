c03d5942186ba8a814a97928b918e572f3428305
DoDaek <dbxor8960@naver.com>
2020-05-16 11:22:56 +0900

    Create main and Set Gotron for application window

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import "github.com/Equanox/gotron"
+
+func main() {
+	// Create a new browser window instance
+	window := app()
+
+	// Start the browser window.
+	// This will establish a golang <=> nodejs bridge using websockets,
+	// to control ElectronBrowserWindow with our window object.
+	done, err := window.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	// Open dev tools must be used after window.Start
+	window.OpenDevTools()
+
+	// Run application(app)
+	runApp(window)
+
+	// Wait for the application to close
+	<- done
+}
+
+func app() *gotron.BrowserWindow {
+	window, err := gotron.New("ui/dist")
+	if err != nil {
+		panic(err)
+	}
+
+	// Alter default window size and window title.
+	window.WindowOptions.Width = 1432
+	window.WindowOptions.Height = 700
+	window.WindowOptions.Title = "Slave's work"
+
+	return window
+}
+
+func runApp(w *gotron.BrowserWindow) {
+	w.On(&gotron.Event{Event: "app.host.start"}, func(bin []byte) {
+		h := newHost(w)
+		go h.run()
+	})
+
+	w.On(&gotron.Event{Event: "app.worker.start"}, func(bin []byte) {
+
+	})
+}
