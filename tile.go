bdced4fa0db4caa16cb6cfd782c005f86b5c6e39
Yutaek Jung <dbxor8960@naver.com>
2020-07-14 14:58:45 +0900

    Implement rendering with slavework's worker. (#28)
    * Implement http handler for blender add-on
    
    * Create file.go for file management
    
    * change file name
    
    * Implement Task tile rendering
    
    * Modified struct Tile's field type

--- tile.go
+++ tile.go
@@ -1,17 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
 
 type Tile struct {
-	Index int `json:"index"`
-	Xmin  int `json:"xmin"`
-	Ymin  int `json:"ymin"`
-	Xmax  int `json:"xmax"`
-	Ymax  int `json:"ymax"`
-	Frame int `json:"fram"`
+	Index string `json:"index"`
+	Xmin  string `json:"xmin"`
+	Ymin  string `json:"ymin"`
+	Xmax  string `json:"xmax"`
+	Ymax  string `json:"ymax"`
+	Frame string `json:"fram"`
+
+	// In JSON parsing, the following parameters are ignored.
+	Active  bool `json:"-"`
+	Success bool `json:"-"`
 }
 
-func (t *Tile) prettyPrint(){
+func (t *Tile) prettyPrint() {
 	fmt.Printf("Tile: { \"index\" : %d, \"xmin\" : %d, \"ymin\" : %d, \"xmax\" : %d, \"ymax\" : %d, \"frame\" : %d}\n", t.Index, t.Xmin, t.Ymin, t.Xmax, t.Ymax, t.Frame)
 }
 
+func (t *Tile) Dispatch(h *Host, path string) {
+	for _, worker := range h.workers {
+		if worker.Status == "" {
+			r, w := io.Pipe()
+			m := multipart.NewWriter(w)
+
+			go func() {
+				defer w.Close()
+				defer m.Close()
+
+				part, err := m.CreateFormFile("blend-file", path)
+				if err != nil {
+					log.Fatal("func : Dispatch\n", err)
+				}
+
+				file, err := os.Open(path)
+				if err != nil {
+					log.Fatal("func : Dispatch\n", err)
+				}
+				defer file.Close()
+
+				if _, err := io.Copy(part, file); err != nil {
+					log.Fatal("func : Dispatch\n", err)
+				}
+			}()
+
+			req, err := http.NewRequest("POST", worker.Address.generateHostAddress("/render/resource"), r)
+			if err != nil {
+				log.Fatal("func : Dispatch\n", err)
+			}
+
+			req.Header.Add("Content-Type", m.FormDataContentType())
+			req.Header.Add("index", t.Index)
+			req.Header.Add("xmin", t.Xmin)
+			req.Header.Add("ymin", t.Ymin)
+			req.Header.Add("xmax", t.Xmax)
+			req.Header.Add("ymax", t.Ymax)
+			req.Header.Add("fram", t.Frame)
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Fatal("func : Dispatch\n", err)
+			}
+
+			resp.Body.Close()
+			worker.Status = "running"
+			break
+		}
+	}
+}
