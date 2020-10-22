364da472c9e3411ae413194f06ce0b164e12f8a4
JJL <rhqnrdl625@naver.com>
2020-10-22 19:40:11 +0900

    task 서브 페이지 css 적용

--- ui/src/componets/tab/sub_tab/Blender.js
+++ ui/src/componets/tab/sub_tab/Blender.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import './Blender.css';
 
 const Blender = () => {
     return (
-        <div className="sub_tab">
-            <h5>Blender</h5>
-            <ul>
-                <li>- Start Blender</li>
-                <li>- Select the "File -> User Preferences" menu item</li>
-                <li>- Switch to tab "Add-ons"</li>
-                <li>- Press the "Install from file..." button in the bottom of the preferences dialog</li>
-                <li>- Select the .zip file and double-click on it</li>
-                <li>- In the search box, enter "bitwrk" to filter the list of add-ons shown</li>
-                <li>- Make sure that "Render: BitWrk Distributed Rendering" has been checked</li>
-                <li>- If this is an update: restart the add-on by un-checking it first</li>
-                <li>- Back in the main window, you should be able to switch the renderer to BitWrk</li>
-            </ul>
+        <div className="sub_tab_background">
+            <div className="sub_tab">
+                <h5 className="sub_tab_title">Blender</h5>
+                <ol>
+                    <li>Start Blender</li>
+                    <li>Select the "File -> User Preferences" menu item</li>
+                    <li>Switch to tab "Add-ons"</li>
+                    <li>Press the "Install from file..." button in the bottom of the preferences dialog</li>
+                    <li>Select the .zip file and double-click on it</li>
+                    <li>In the search box, enter "bitwrk" to filter the list of add-ons shown</li>
+                    <li>Make sure that "Render: BitWrk Distributed Rendering" has been checked</li>
+                    <li>If this is an update: restart the add-on by un-checking it first</li>
+                    <li>Back in the main window, you should be able to switch the renderer to BitWrk</li>
+                </ol>
+            </div>
         </div>
     );
 }
